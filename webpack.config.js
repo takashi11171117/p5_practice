860c72afa478e171792da253f2e2ef211ec5cb28
Takashi Saito <takashi11171117@gmail.com>
2019-10-31 23:48:19 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      "node_modules"
+    ],
+    extensions: [".ts", ".js"]
+  }
+};
