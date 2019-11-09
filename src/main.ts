d68ea20426e8a5cab19152ae61010e546a6c49d0
Takashi Saito <takashi11171117@gmail.com>
2019-11-09 23:45:10 +0900

    Added mass

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,44 @@
 import * as p5 from 'p5';
 
 type MoverType = {
+    applyForce: Function
     update: Function
     display: Function
     checkEdges: Function
 }
 
-let Mover = (p: p5) => {
-    let location = p.createVector(p.random(p.width), p.random(p.height))
+let Mover = (p: p5, m: number, x: number, y: number) => {
+    let location = p.createVector(x, y)
     let velocity = p.createVector(0, 0)
-    let acceleration = p.createVector(-0.001, 0.01)
-    let topspeed = 4
+    let acceleration = p.createVector(0, 0)
+    let mass = m
 
     return {
+        applyForce: (force: p5.Vector) => {
+            let f = p5.Vector.div(force, mass)
+            acceleration.add(f)
+        },
         update: () => {
-            let mouse = p.createVector(p.mouseX, p.mouseY)
-            let dir = p5.Vector.sub(mouse, location)
-
-            dir.normalize()
-            dir.mult(0.5)
-            acceleration = dir
-
             velocity.add(acceleration)
-            velocity.limit(topspeed)
             location.add(velocity)
+            acceleration.mult(0)
         },
         display: () => {
             p.stroke(0)
             p.fill(175)
-            p.ellipse(location.x, location.y, 70, 70)
+            p.ellipse(location.x, location.y, mass * 16, mass * 16)
         },
         checkEdges: () => {
             if (location.x > p.width) {
-                location.x = 0
-            } else if (location.x < 0) {
                 location.x = p.width
+                velocity.x *= -1
+            } else if (location.x < 0) {
+                velocity.x *= -1
+                location.x = 0
             }
 
             if (location.y > p.height) {
-                location.y = 0
-            } else if (location.y > p.height) {
+                velocity.y *= -1
                 location.y = p.height
             }
         }
@@ -54,7 +53,7 @@ const sketch = (p: p5) => {
 
     p.setup = () => {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
-        [...Array(20)].map(() => movers.push(Mover(p)))
+        [...Array(100)].map(() => movers.push(Mover(p, p.random(0.1, 5), 0, 0)))
     };
 
     p.windowResized = () => {
@@ -62,11 +61,17 @@ const sketch = (p: p5) => {
     };
 
     p.draw = () => {
-        p.background(0);
-        [...Array(20)].map((_, i) => {
+        p.background(255);
+
+        let wind = p.createVector(0.01, 0)
+        let gravity = p.createVector(0, 0.1);
+
+        [...Array(100)].map((_, i) => {
+            movers[i].applyForce(wind)
+            movers[i].applyForce(gravity)
             movers[i].update()
-            movers[i].checkEdges()
             movers[i].display()
+            movers[i].checkEdges()
         })
     }
 };
